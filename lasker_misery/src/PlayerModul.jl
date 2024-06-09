5419e523274460409e2c4c373f30ded0965ac4c6
Karl Linek <karl@linek.at>
2024-06-09 15:41:54 +0200

    added playerModul.jl

---
+++ lasker_misery/src/PlayerModul.jl
@@ -0,0 +1,24 @@
+module PlayerModule
+
+export Player, input_Players
+
+mutable struct Player
+    name::String
+end
+
+function create_player(num::Int)::Player
+    while true
+        println("Player$num, wie lautet ihr Name?")
+        _player_name = readline()
+        if all(isletter, _player_name)
+            return Player(_player_name)
+        end
+    end
+end
+
+function input_Players()::Vector{Player}
+    players::Vector{Player} = [create_player(num) for num in 1:2]
+    return players
+end
+end # module
+
